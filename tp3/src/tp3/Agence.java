48bd4723832d3d93ac0106c7d03af3aba568c5b5
unknown <brahimvall.boumouzouna@etu.fstm.ac.ma>
2022-12-16 18:59:39 +0100

    add selection and show selection methods

--- tp3/src/tp3/Agence.java
+++ tp3/src/tp3/Agence.java
@@ -1,12 +1,28 @@
 package tp3;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 public class Agence {
 	private List<Voiture> voitures;
 	public Agence()
 	{
 		voitures= new ArrayList<>();
 	}
-	public void selectionne (Critere c) {}
-	public void afficheSelection(Critere c) {}
+	public Iterator<Voiture> selectionne (Critere c) {
+		ArrayList<Voiture> tmp=new ArrayList<>();
+		for (Voiture voiture : voitures) {
+			if(c.estSatisfaitPar(voiture)) {
+				tmp.add(voiture);
+			}
+		}
+		return tmp.iterator();
+	}
+	public void afficheSelection(Critere c) {
+		Iterator<Voiture> elements=selectionne(c);
+		while(elements.hasNext())
+		{
+			System.out.println(elements.next());
+		}
+		
+	}
 }

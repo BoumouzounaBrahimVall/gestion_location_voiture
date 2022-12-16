e301b31e32315cc32aa2cf7e769185dac7c80f90
unknown <brahimvall.boumouzouna@etu.fstm.ac.ma>
2022-12-16 18:21:20 +0100

    add agence class

---
+++ tp3/src/tp3/Agence.java
@@ -0,0 +1,12 @@
+package tp3;
+import java.util.ArrayList;
+import java.util.List;
+public class Agence {
+	private List<Voiture> voitures;
+	public Agence()
+	{
+		voitures= new ArrayList<>();
+	}
+	public void selectionne (Critere c) {}
+	public void afficheSelection(Critere c) {}
+}

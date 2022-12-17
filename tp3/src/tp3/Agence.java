496c6f0e4f8ac16601871dd3b4f7a0cd69d61470
unknown <brahimvall.boumouzouna@etu.fstm.ac.ma>
2022-12-17 21:16:59 +0100

    completing agence class

--- tp3/src/tp3/Agence.java
+++ tp3/src/tp3/Agence.java
@@ -1,13 +1,18 @@
 package tp3;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Iterator;
+import java.util.Map;
 public class Agence {
 	private List<Voiture> voitures;
+	private Map<Client, Voiture> locations ;
 	public Agence()
 	{
 		voitures= new ArrayList<>();
+		locations=new HashMap<>();
 	}
+	
 	public Iterator<Voiture> selectionne (Critere c) {
 		ArrayList<Voiture> tmp=new ArrayList<>();
 		for (Voiture voiture : voitures) {
@@ -29,4 +34,49 @@ public class Agence {
 		voitures.add(v);
 	}
 	
+	public void loueVoiture(Client client, Voiture v) {
+		
+		try {
+			if(voitures.contains(v))  
+			{
+				try {
+					if(!estLoue(v)) {
+						
+						try {
+							if(!estLoueur(client))
+							{
+								locations.put(client, v); 
+							}
+						}catch (Exception e) {
+							System.out.println("client loueur ");
+						}
+						
+					}else throw new Exception();
+				}catch (Exception e) {
+					System.out.println("la voiture est deja loue ");
+				}
+				
+			}else throw new Exception();
+		}catch(Exception ignored)
+		{
+			System.out.println("la voiture n'est pas dans l'agence ");
+		}
+	}
+	
+	public boolean estLoueur(Client client) {
+			return locations.containsKey(client);
+	}
+	public boolean estLoue(Voiture v) {
+		return locations.containsValue(v);
+	}
+	public void rendVoiture(Client client) {
+		if(estLoueur(client)) locations.remove(client);	
+	}
+	public Iterator<Voiture> lesVoituresLouees() {
+		List<Voiture> tmp=new ArrayList<>();
+		for (Map.Entry<Client,Voiture> entry : locations.entrySet()) {
+			tmp.add(entry.getValue());
+		}
+		return tmp.iterator();
+	}
 }

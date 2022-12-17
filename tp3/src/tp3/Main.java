a80d153a33baa7f3ac8f68260f2eecada02c8956
unknown <brahimvall.boumouzouna@etu.fstm.ac.ma>
2022-12-17 18:39:04 +0100

    add price critere class

--- tp3/src/tp3/Main.java
+++ tp3/src/tp3/Main.java
@@ -8,12 +8,16 @@ public class Main {
 		agence.ajouterVoiture(new Voiture("BMW", "A5", 2018, 256));
 		agence.ajouterVoiture(new Voiture("Audi", "Q7", 2016, 86));
 		agence.ajouterVoiture(new Voiture("Mazerati", "M23A", 2017, 100));
-		agence.ajouterVoiture(new Voiture("Mercides", "190", 1996, 45));
+		agence.ajouterVoiture(new Voiture("Mercides", "190D", 1996, 45));
 		
+		agence.ajouterVoiture(new Voiture("Renaut", "21", 2009, 56));
+		agence.ajouterVoiture(new Voiture("Renaut", "2C34", 2009, 146));
+		agence.ajouterVoiture(new Voiture("Renaut", "2008", 2021, 170));
 		
 		InterCritere crit=new InterCritere();
-		crit.addCritere(new CritereMarque("BMW"));
+		crit.addCritere(new CritereMarque("Renaut"));
 		crit.addCritere(new CriterePrix(120));
+		crit.addCritere(new CritereAnnee(2009));
 		
 		agence.afficheSelection(crit);
 

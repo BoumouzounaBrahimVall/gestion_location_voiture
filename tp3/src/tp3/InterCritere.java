628181e6b2b2991842ee4e031ea26e50438b5938
unknown <brahimvall.boumouzouna@etu.fstm.ac.ma>
2022-12-17 15:28:51 +0100

    adding multi critere class

---
+++ tp3/src/tp3/InterCritere.java
@@ -0,0 +1,24 @@
+package tp3;
+import java.util.ArrayList;
+import java.util.List;
+public class InterCritere  implements Critere{
+	
+	private List<Critere> lesCriteres;
+	
+	public InterCritere() {
+		lesCriteres=new ArrayList<>();
+	}
+	public void addCritere(Critere c)
+	{
+		lesCriteres.add(c);
+	}
+	@Override
+	public boolean estSatisfaitPar(Voiture v) {
+		for(Critere crit : lesCriteres)
+		{
+			if(!crit.estSatisfaitPar(v)) return false;
+		}
+		return true;
+	}
+
+}

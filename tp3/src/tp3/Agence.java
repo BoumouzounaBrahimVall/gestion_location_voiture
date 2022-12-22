e5e794e96b457ef437966f13086151f544054969
S USER <brahimvall.boumouzouna@etu.fstm.ac.ma>
2022-12-22 11:30:31 +0100

    added a methode to agence class

--- tp3/src/tp3/Agence.java
+++ tp3/src/tp3/Agence.java
@@ -1,4 +1,5 @@
 package tp3;
+
 import java.util.ArrayList;
 //import java.util.HashMap;
 import java.util.List;
@@ -8,83 +9,105 @@ import java.util.TreeMap;
 
 public class Agence {
 	private List<Voiture> voitures;
-	private Map<Client, Voiture> locations ;
-	public Agence()
-	{
-		voitures= new ArrayList<>();
-		locations=new TreeMap<>();
+	private Map<Client, Voiture> locations;
+
+	public Agence() {
+		voitures = new ArrayList<>();
+		locations = new TreeMap<>();
 	}
-	
-	public Iterator<Voiture> selectionne (Critere c) {
-		ArrayList<Voiture> tmp=new ArrayList<>();
+
+	public Iterator<Voiture> selectionne(Critere c) {
+		ArrayList<Voiture> tmp = new ArrayList<>();
 		for (Voiture voiture : voitures) {
-			if(c.estSatisfaitPar(voiture)) {
+			if (c.estSatisfaitPar(voiture)) {
 				tmp.add(voiture);
 			}
 		}
 		return tmp.iterator();
 	}
+
 	public void afficheSelection(Critere c) {
-		Iterator<Voiture> elements=selectionne(c);
-		while(elements.hasNext())
-		{
+		Iterator<Voiture> elements = selectionne(c);
+		while (elements.hasNext()) {
 			System.out.println(elements.next());
 		}
-		
+
 	}
+
 	public void ajouterVoiture(Voiture v) {
 		voitures.add(v);
 	}
-	
+
 	public void loueVoiture(Client client, Voiture v) {
-		
+
 		try {
-			if(voitures.contains(v))  
-			{
+			if (voitures.contains(v)) {
 				try {
-					if(!estLoue(v)) {
-						
+					if (!estLoue(v)) {
+
 						try {
-							if(!estLoueur(client))
-							{
-								locations.put(client, v); 
-							}else throw new Exception();
-						}catch (Exception e) {
+							if (!estLoueur(client)) {
+								locations.put(client, v);
+							} else
+								throw new Exception();
+						} catch (Exception e) {
 							System.out.println("client loueur ");
 						}
-						
-					}else throw new Exception();
-				}catch (Exception e) {
+
+					} else
+						throw new Exception();
+				} catch (Exception e) {
 					System.out.println("la voiture est deja loue ");
 				}
-				
-			}else throw new Exception();
-		}catch(Exception ignored)
-		{
+
+			} else
+				throw new Exception();
+		} catch (Exception ignored) {
 			System.out.println("la voiture n'est pas dans l'agence ");
 		}
 	}
-	
+
 	public boolean estLoueur(Client client) {
-			return locations.containsKey(client);
+		return locations.containsKey(client);
 	}
+
 	public boolean estLoue(Voiture v) {
 		return locations.containsValue(v);
 	}
+
 	public void rendVoiture(Client client) {
-		if(estLoueur(client)) locations.remove(client);	
+		if (estLoueur(client))
+			locations.remove(client);
 	}
+
 	public Iterator<Voiture> lesVoituresLouees() {
-		List<Voiture> tmp=new ArrayList<>();
-		for (Map.Entry<Client,Voiture> entry : locations.entrySet()) {
-			tmp.add(entry.getValue());
-		}
-		return tmp.iterator();
+		List<Voiture> voituresLouees = new ArrayList<Voiture>();
+		voituresLouees.addAll(this.locations.values());
+		return voituresLouees.iterator();
 	}
-	public void afficherLocation()
+
+	public Iterator<Client> lesClientsLoueur() {
+		List<Client> clientsLoueur = new ArrayList<Client>();
+		clientsLoueur.addAll(this.locations.keySet());
+		return clientsLoueur.iterator();
+	}
+
+	public void affichClientsLoueur()
 	{
+		Iterator<Client> iter_clients = this.lesClientsLoueur();
+		while (iter_clients.hasNext())
+			System.out.println(iter_clients.next());
+	}
+
+	public void affichVoituresLouees()
+	{
+		Iterator<Voiture> iter_voitures = this.lesVoituresLouees();
+		while (iter_voitures.hasNext())
+			System.out.println(iter_voitures.next());
+	}
+
+	public void afficherLocation() {
 		locations.forEach(
-				(k,v)->System.out.println("---------------------\nClient: \n\t"+
-											k+"\nVoiture loue: \n\t"+v));
+				(k, v) -> System.out.println("---------------------\nClient: \n\t" + k + "\nVoiture loue: \n\t" + v));
 	}
 }

f1c814e093aa4c5a7ccb89ef4bde77ee36f2e5f3
BoumouzounaBrahimVall <brahimvall.boumouzouna@etu.fstm.ac.ma>
2023-01-18 15:03:14 +0100

    added interface welcome label and buttons

--- tp3/src/tp6/NosCouleur.java
+++ tp3/src/tp6/NosCouleur.java
@@ -3,7 +3,7 @@ package tp6;
 import java.awt.Color;
 
 public interface NosCouleur {
-	Color COLOR1 =new Color(0xb0bcd4);// light
+	Color COLOR1 =new Color(0xd8e1f0);// light
 	Color COLOR2 =new Color(0x7d8597);// strong
 	Color COLOR3 = new Color(0x33415c);// very Strong
 	Color COLOR4 =new Color(0xffffff);
--- tp3/src/tp6/interfaceAccueil.java
+++ tp3/src/tp6/interfaceAccueil.java
@@ -1,9 +1,15 @@
 package tp6;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class interfaceAccueil extends JPanel implements ActionListener{
@@ -12,13 +18,51 @@ public class interfaceAccueil extends JPanel implements ActionListener{
 	private InterfacePrincipale IP;
 	public interfaceAccueil(InterfacePrincipale ip)
 	{
+		
 		IP=ip;
+		this.setLayout(new GridBagLayout());
+		JPanel panelBtns=new JPanel(new  GridLayout(1,2,20,20));
+		JPanel panelTextBtn=new JPanel(new BorderLayout());
+		panelTextBtn.setBackground(NosCouleur.COLOR1);
+
+		JLabel titre=new JLabel("Bienvenu Dans H.V Location");
+		titre.setFont(new Font("",Font.BOLD,60));
+		titre.setBackground(NosCouleur.COLOR1);
+		titre.setForeground(NosCouleur.COLOR3);
+
+		Font f=new Font("",Font.BOLD,30);
+		
 		voitures=new JButton("Gestion Voitures");
+		voitures.setIcon(new ImageIcon("car.png"));
+		voitures.setHorizontalTextPosition(JLabel.CENTER);
+		voitures.setVerticalTextPosition(JLabel.BOTTOM);
+		voitures.setFont(f);
+		voitures.setBackground(NosCouleur.COLOR3);
+		voitures.setForeground(NosCouleur.COLOR1);
+		
 		location=new JButton("Gestion Locations");
+		location.setIcon(new ImageIcon("carRent.png"));
+		location.setHorizontalTextPosition(JLabel.CENTER);
+		location.setVerticalTextPosition(JLabel.BOTTOM);
+		location.setFont(f);
+		location.setBackground(NosCouleur.COLOR3);
+		location.setForeground(NosCouleur.COLOR1);
+
+
+			
+		
 		voitures.addActionListener(this);
 		location.addActionListener(this);
-		this.add(voitures);
-		this.add(location);
+		panelBtns.add(voitures);
+		panelBtns.add(location);
+		panelBtns.setBackground(NosCouleur.COLOR1);
+		
+		panelTextBtn.add(titre,BorderLayout.NORTH);
+		panelTextBtn.add(panelBtns,BorderLayout.CENTER);
+
+		this.add(panelTextBtn);
+		this.setBackground(NosCouleur.COLOR1);
+		
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
